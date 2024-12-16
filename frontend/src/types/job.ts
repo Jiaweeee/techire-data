b5b5e4a6cd4bf08853c574d3c65d3483b598f678
jiawei.an <ajw211@163.com>
2024-12-16 10:53:16 +0800

    refactor: remove unused Job and SearchResponse interfaces from job.ts

--- frontend/src/types/job.ts
+++ frontend/src/types/job.ts
@@ -1,21 +1,3 @@
-export interface Job {
-  id: string;
-  title: string;
-  company: string;
-  location: string;
-  salary: string;
-  description: string;
-  isRemote: boolean;
-  postedAt: string;
-}
-
-export interface SearchResponse {
-  jobs: Job[];
-  total: number;
-  page: number;
-  perPage: number;
-}
-
 export enum JobSortBy {
   RELEVANCE = 0,
   DATE = 1
